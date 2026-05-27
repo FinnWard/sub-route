0f09b268c3d0f7fe5cd58fac42aba97ac32850c7
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2026-05-27 16:22:40 +0200

    clean up next config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/docs",
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:8080", "127.0.0.1:8080"],
-    },
-  },
   output: "standalone",
   reactCompiler: true,
 };
