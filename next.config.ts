160b389c4bc60157fecf7524657b96597577acf2
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2026-05-27 15:00:27 +0200

    fix local dev for server actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/docs",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:8080", "127.0.0.1:8080"],
+    },
+  },
   output: "standalone",
   reactCompiler: true,
 };
